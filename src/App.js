bf50ca903d0d51562fb027e6726c1be2f53a753f
Johanne Lie <johanlie@stud.ntnu.no>
2019-10-14 16:55:43 +0200

    Make Tab resolve issue #6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from "react";
+
+import React, { useState, useEffect } from "react";
+import Tabs from "./components/Tabs";
 
 function App() {
+
+  const [activeTab, setActiveTab] = useState("0");
+
+  
+  function changeActiveTab(active) {
+    setActiveTab(active);
+  }
+
   return (
     <div className="App">
       <h1>Vinmonopol</h1>
+      <Tabs
+          changeActiveTab={changeActiveTab}
+          active={activeTab}
+        />
+       
     </div>
   );
 }
